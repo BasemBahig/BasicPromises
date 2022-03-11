53282d07c5667001b9e6b57817952ba705152f3f
BasemHennawi <60872972+BasemBahig@users.noreply.github.com>
2022-03-11 12:50:25 +0200

    Try and Catch test

--- index.js
+++ index.js
@@ -6,33 +6,54 @@
     console.log(data)
 }*/
 const Base_URL="https://jsonplaceholder.typicode.com/"
+
 function getPhoto(){
     //https://jsonplaceholder.typicode.com/posts
-    fetch(`https://jsonplaceholder.typicode.com/photos`)
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
     .then(response => response.json()) 
     .then(data=>{
-        console.log(data)
+        console.log(data, Base_URL)
     })
 }
 getPhoto()
-  
-/*try{
-const response = await fetch(input `${Base_URL}/posts`)
+//Update API
+const UpdatemyAPI=(data)=>{
+    //console.log(data)
+    const result= document.getElementById('result')
+    const fragment = document.createDocumentFragment()
+    console.log(result)
+    //<li id="item-1"></li>
+    const li=document.createElement('li')
+    //li.textContent='Text'
+    result.appendChild(li)
+    data.forEach(data => {
+        const li=document.createElement('li')
+        li.textContent=data.title
+        result.appendChild(li)
+    fragment.appendChild(li)
+        
+    });
+result.appendChild(fragment)
+}
+//Try and catch
+ async function ReadMyAPI(){
+try{
+const response = await fetch(`https://jsonplaceholder.typicode.com/photos`)
 if (response.status ===200){
     const data = await response.json()
-    console.log(data);
+    console.log(response);
+    UpdatemyAPI(data)
 
-    UpdatemyAPI()
+    
 }       
 console.log(response)
 }catch(e) {
     console.log(e)
+    console.log('error')
 }
 
-}*/
-
-/*getPhoto()*/  
-
+}
+ReadMyAPI()
 /*const promise = new Promise(function(resolve, reject){
     var img = document.createElement('img');
     img.src = 'img.jpg';
